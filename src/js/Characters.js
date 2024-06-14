40ae79d5f6e679b13d20ab240d6e5265df27344e
Arrtish2702 <arrtishm@gmail.com>
2024-06-15 00:14:11 +0800

    modified: adapted css for page to look cleaner without most uncessary borders and implemented first round of coloured css for styling look. Also changed background images for ac box , hp box and magic box

--- src/js/Characters.js
+++ src/js/Characters.js
@@ -5,29 +5,29 @@ import '../css/Characters.css';
 const Characters = () => {
   return (
     <div className='characters-container'>
-      <div className='main-col-1 bordered'>
-        <div className='profile-row bordered'>
-          <div className='profile-section bordered'>
-            <div className='profile-pic bordered'></div>
-            <div className='charc-intro bordered'>
+      <div className='main-col-1 padded'>
+        <div className='profile-row '>
+          <div className='profile-section padded'>
+            <div className='profile-pic bordered-black '></div>
+            <div className='charc-intro bordered-black padded celadon_green'>
                 Nicol Bolas
             </div>
           </div>
-          <div className='hp-ac-section bordered'>
+          <div className='hp-ac-section padded'>
             <table className='hp-ac-table'>
                 <tbody>          
                     <tr>
-                        <td>50/50</td>
+                        <td className=' hp-bckground celadon_green'>50/50</td>
                     </tr>
                     <tr>
-                        <td>16</td>
+                        <td className=' ac-bckground pastel_pink'>16</td>
                     </tr>
                 </tbody>
             </table>
           </div>
         </div>
-        <div className='stats-row bordered'>
-          <div className='score-col bordered'>
+        <div className='stats-row padded'>
+          <div className='score-col dark_green'>
             <table className='stats-table'>
                 <tbody>          
                     <tr>
@@ -57,23 +57,23 @@ const Characters = () => {
                 </tbody>
             </table>
           </div>
-          <div className='skill-col bordered'>
-            <div className='profs-list'>
+          <div className='skill-col '>
+            <div className='profs-list bordered-black dark_green'>
                 <h3>Use Magical Device</h3>
             </div>
           </div>
         </div>
       </div>
-      <div className='main-col-2 bordered'>
-        <div className='bonu bordered'>
+      <div className='main-col-2 padded'>
+        <div className='bonuses-row'>
             <table className='bonuses-table'>
                 <tbody>          
                     <tr>
-                        <td className='speed-init'>+2</td>
-                        <td className='atk-bonus'>+5</td>
-                        <td className='spell-no'>+8</td>
+                        <td className='speed-init pink'>+2</td>
+                        <td className='atk-bonus pink'>+5</td>
+                        <td className='spell-no pink'>+8</td>
                         <td>
-                            <table className='magic-items-table'>
+                            <table className='magic-items-table pink'>
                                 <tbody>
                                     <tr>
                                         <td>Magic Item 1</td>
@@ -91,7 +91,7 @@ const Characters = () => {
                 </tbody>
             </table>
         </div>
-        <div className='row-2-2 bordered'></div>
+        <div className='extra-details bordered-black padded celadon_green'></div>
       </div>
       <div><Link to="/" className="button">Back to Home</Link></div>
     </div>
