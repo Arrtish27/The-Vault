24980306364e042b0f747cf7d3759cc285da1951
Arrtish2702 <arrtishm@gmail.com>
2024-06-16 01:54:10 +0800

    authentication for google
    Implemented login for users using google account authentication.

---
+++ src/js/AuthContext.js
@@ -0,0 +1,25 @@
+// src/context/AuthContext.js
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase.config.js';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
